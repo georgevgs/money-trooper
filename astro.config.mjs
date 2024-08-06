3955665ebfb258ead0f28766e1d4365724cb9201
George Vagdas <dev@vagdas.eu>
2024-08-07 00:22:30 +0300

    Using netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
   integrations: [react(), tailwind(), db()],
+  adapter: netlify()
 });
