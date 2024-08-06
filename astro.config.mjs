c5997f6e2cf15023d9760105588aaec443ee3504
George Vagdas <dev@vagdas.eu>
2024-08-06 23:48:00 +0300

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-import vercel from '@astrojs/vercel/serverless';
 import dotenv from 'dotenv';
+import netlify from "@astrojs/netlify";
 
 dotenv.config();
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react(), tailwind(), db()],
-  adapter: vercel(),
+  adapter: netlify()
 });
