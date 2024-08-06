c382dd086aec9c9515bf092e6e5fac0bbde2f397
George Vagdas <dev@vagdas.eu>
2024-08-07 00:11:47 +0300

    Changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-import dotenv from 'dotenv';
 import netlify from "@astrojs/netlify";
-
-dotenv.config();
 
 export default defineConfig({
   output: 'server',
